f61df276d602a698b596bc203ea8665be4b611db
Stalyn Rosario <59269402+Stalyn19@users.noreply.github.com>
2022-06-28 19:06:40 -0400

    Payment update
    class payment

--- src/main/java/com/hotelreservation/simpreserv/Payment.java
+++ src/main/java/com/hotelreservation/simpreserv/Payment.java
@@ -6,12 +6,31 @@ import lombok.Getter;
 import java.util.Date;
 
 public class Payment {
+    @Setter @Getter
     private long id;
+    @Setter @Getter
     private int idReservation;
+    @Setter @Getter
     private Voucher voucher;
+    @Setter @Getter
     private double tax;
+    @Setter @Getter
     private double fullPayment;
+    @Setter @Getter
     private Date issueDate;
+    @Setter @Getter
     private Date paymentDay;
 
+    public Payment(long id, int idReservation, Voucher voucher, double tax, double fullPayment, Date issueDate, Date paymentDay) {
+        this.id = id;
+        this.idReservation = idReservation;
+        this.voucher = voucher;
+        this.tax = tax;
+        this.fullPayment = fullPayment;
+        this.issueDate = issueDate;
+        this.paymentDay = paymentDay;
+    }
+
+    public Payment() {
+    }
 }
