1261a293dce3a6ac146bf23c7a41049f9123defd
Stalyn Rosario <59269402+Stalyn19@users.noreply.github.com>
2022-06-28 19:19:09 -0400

    Update Voucher.java
    Update

--- src/main/java/com/hotelreservation/simpreserv/Voucher.java
+++ src/main/java/com/hotelreservation/simpreserv/Voucher.java
@@ -2,20 +2,26 @@ package com.hotelreservation.simpreserv;
 import lombok.Setter;
 import lombok.Getter;
 
+import java.util.Date;
 import java.util.List;
 
 public class Voucher {
     @Setter @Getter
     private long voucherId;
-
+    @Setter @Getter
+    private Date issueDate;
+    @Setter @Getter
+    private Payment paymentAmount;
     @Setter @Getter
     private Client client;
-
     @Setter @Getter
     private Employee employee;
 
-    public Voucher(long voucherId, Client client, Employee employee) {
+
+    public Voucher(long voucherId, Date issueDate, Payment paymentAmount, Client client, Employee employee) {
         this.voucherId = voucherId;
+        this.issueDate = issueDate;
+        this.paymentAmount = paymentAmount;
         this.client = client;
         this.employee = employee;
     }
@@ -25,7 +31,11 @@ public class Voucher {
     }
 
     public List<String > generateVoucher(List<String> list){
-
+      list.add(String.valueOf(this.getVoucherId()));
+      //this.getIssueDate().setTime(22/5/1958);
+        list.add(String.valueOf(this.getPaymentAmount().getFullPayment()));
+        //list.add(this.getClient().getName());
+        //list.add(this.getEmployee().getName());
         return list;
     }
 }
