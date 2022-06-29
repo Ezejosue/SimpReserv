caa14550998749f3a26b6cfc50fe3edf094134ba
Stalyn Rosario <59269402+Stalyn19@users.noreply.github.com>
2022-06-29 10:06:14 -0400

    Update Menu.java
    update menu

--- src/main/java/com/hotelreservation/simpreserv/Menu.java
+++ src/main/java/com/hotelreservation/simpreserv/Menu.java
@@ -65,10 +65,65 @@ public class Menu {
                     break;
                 }
             }
-        }while (close!=6);
+        }while (close!=0);
     }
 
     public void employeeMenu(){
+        int close=0;
+        do{
+            System.out.println("SimpReserv 1.0");
+            System.out.println("Bienvenido al sistema de registro del Hotel Hilton");
+            System.out.println("Opciones de gestion de Huespedes");
+            System.out.println("1-Registrar Usuario");
+            System.out.println("2-Reservar Habitacion");
+            System.out.println("3-Cancelar reserva");
+            System.out.println("4-Solicitar Membrecia");
+            System.out.println("5-Cancelar Membrecia");
+            System.out.println("6-Salir");
+            int opt= sc.nextInt();
+            switch (opt){
 
+                case 1:{//Registrar Usuario
+                    System.out.println("Opciones de registro de usuarios");
+                    System.out.println("Nombre");
+                    System.out.println("Apellido");
+                    System.out.println("Tipo de documento de identidad");
+                    System.out.println("Numero de documento de identidad");
+                    System.out.println("Fecha de nacimiento");
+                    System.out.println("Genero");
+                    System.out.println("Direccion");
+                    System.out.println("Telefono");
+                    System.out.println("Correo Electronico");
+                    break;
+                }
+
+                case 2:{//Reservar Habitacion
+                    System.out.println("Confirmar Seleccion del Huesped");
+                    System.out.println("Procesar pago");
+                    break;
+                }
+                case 3:{//Cancelar reserva
+                    System.out.println("Cancelar Reserva del huesped");
+
+                    break;
+                }
+                case 4:{//Solicitar Membrecia
+                    System.out.println("Procesar solicitud de membrecia");
+                    System.out.println("Enviar correo electronico de confirmacion");
+
+                    break;
+                }
+                case 5:{//Cancelar Membrecia
+                    System.out.println("Cancelar membrecia");
+                    System.out.println("Enviar enviar correo de confirmacion");
+                    break;
+                }
+                case 6:{//Cerrar la applicacion
+                    System.out.println("La aplicacion se ha cerrado con exito");
+                    opt=0;
+                    break;
+                }
+            }
+        }while (close!=0);
     }
 }
