5a18c0b6f551cd04632ed2cea2243bfd57e988d6
Stalyn Rosario <59269402+Stalyn19@users.noreply.github.com>
2022-06-29 10:25:20 -0400

    implementation
    interface

--- src/main/java/com/hotelreservation/simpreserv/Client.java
+++ src/main/java/com/hotelreservation/simpreserv/Client.java
@@ -1,6 +1,32 @@
 package com.hotelreservation.simpreserv;
 
-public class Client {
+import entity.PaymentInterface;
+
+public class Client implements PaymentInterface {
 
 
+    @Override
+    public String viewPoints() {
+        return null;
+    }
+
+    @Override
+    public void applyPoints() {
+
+    }
+
+    @Override
+    public void setMembership() {
+
+    }
+
+    @Override
+    public void cancelMembership() {
+
+    }
+
+    @Override
+    public void updateMembership() {
+
+    }
 }
--- src/main/java/com/hotelreservation/simpreserv/Employee.java
+++ src/main/java/com/hotelreservation/simpreserv/Employee.java
@@ -1,4 +1,30 @@
 package com.hotelreservation.simpreserv;
 
-public class Employee {
+import entity.PaymentInterface;
+
+public class Employee implements PaymentInterface {
+    @Override
+    public String viewPoints() {
+        return null;
+    }
+
+    @Override
+    public void applyPoints() {
+
+    }
+
+    @Override
+    public void setMembership() {
+
+    }
+
+    @Override
+    public void cancelMembership() {
+
+    }
+
+    @Override
+    public void updateMembership() {
+
+    }
 }
