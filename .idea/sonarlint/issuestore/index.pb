24339c50d489060d588c59396e40742dd816c43e
Stalyn Rosario <59269402+Stalyn19@users.noreply.github.com>
2022-06-30 09:35:13 -0400

    Done
    update voucher

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+H
+src/main/java/Email.java,4\8\486611a510a59f0718c23517e01d2ded71b181ff
+i
+9src/main/java/com/hotelreservation/simpreserv/Person.java,9\c\9ca77b680fa823584ab2cc48b3b418f3d403caf0
+V
+&src/main/java/enums/employeeState.java,a\1\a111ad7f122ff61402c66d46c4aa1b836325cd91
+q
+Asrc/main/java/com/hotelreservation/simpreserv/MembershipType.java,e\7\e7665e781c7edb106e81ea66582f6b79ee2b42cd
+g
+7src/main/java/com/hotelreservation/simpreserv/Menu.java,a\4\a4a70e2e8c9829bb350ec2e5d87c3883353f108d
+X
+(src/main/java/entity/ProcessPayment.java,9\a\9ad92b2799b7b746fd3db0b5826366b61e07de0d
--- src/main/java/com/hotelreservation/simpreserv/Voucher.java
+++ src/main/java/com/hotelreservation/simpreserv/Voucher.java
@@ -10,33 +10,17 @@ public class Voucher {
   @Setter @Getter private long voucherId;
   @Setter @Getter private  Date issueDate;
   @Setter @Getter private Payment paymentAmount;
-  @Setter @Getter private Client client;
-  @Setter @Getter private Employee employee;
+ // @Setter @Getter private Client client;
+//  @Setter @Getter private Employee employee;
 
   public Voucher(double amount) {
     this.voucherId++;
     this.paymentAmount=new Payment(amount);
-    this.client.getName();
-    this.employee.getName();
+   // this.client.setName(client);
+   // this.employee.setName(employee);
     this.issueDate = new Date();
   }
 
-
-  public List<String> generateVoucher(List<String> list) {
-    Client cl=new Client();
-    cl.setName("Andres");
-    this.setClient(cl);
-    Employee emp=new Employee();
-    cl.setName("Josue");
-    this.setEmployee(emp);
-
-    list.add(String.valueOf(this.getVoucherId()));
-    list.add(String.valueOf(this.paymentAmount.getTotalPayment()));
-    list.add(this.getClient().getName());
-    list.add(this.getEmployee().getName());
-    list.add(this.getIssueDate().toString());
-    return list;
-  }
 
   @Override
   public String toString() {
@@ -44,22 +28,14 @@ public class Voucher {
             "voucherId=" + voucherId +
             ", issueDate=" + issueDate +
             ", paymentAmount=" + paymentAmount +
-            ", client=" + client +
-            ", employee=" + employee +
             '}';
   }
 
   public static void main(String[] args) {
 
+    Voucher vaucher=new Voucher(356.89);
 
-    Voucher va=new Voucher(356.98);
-
-    List<String >list=new ArrayList<>();
-
-    list=va.generateVoucher(list);
-
-    list.get(0);
-    System.out.println(list.get(0));
+    System.out.println(vaucher);
   }
 
 }
