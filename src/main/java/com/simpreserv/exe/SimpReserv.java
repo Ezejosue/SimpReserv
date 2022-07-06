c4d94564a51cce131a1a9bea642592a2d2313506
Tony Vasquez <tonyva002@hotmail.com>
2022-07-06 10:40:04 -0400

    Create file SimpReserv.txt

--- src/main/java/com/simpreserv/exe/SimpReserv.java
+++ src/main/java/com/simpreserv/exe/SimpReserv.java
@@ -5,7 +5,12 @@ import com.hotelreservation.simpreserv.*;
 public class SimpReserv {
 
   public static void main(String[] args) {
+
+    CreateFile createFile = new CreateFile();
+    createFile.setCreateFile();
+
     Menu login = new Menu();
     login.initialize();
+
   }
 }
