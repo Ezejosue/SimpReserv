14e3bf4a07133a575ee7639d064181b930c1c5c3
Tony Vasquez <tonyva002@hotmail.com>
2022-07-06 10:18:03 -0400

    Update createFile

--- src/main/java/com/hotelreservation/simpreserv/CreateFile.java
+++ src/main/java/com/hotelreservation/simpreserv/CreateFile.java
@@ -5,32 +5,31 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CreateFile {
+    public void setCreateFile() {
 
-  public static void main(String[] args) {
+        try {
+            File myFile = new File("SimpReserv.txt");
+            if (myFile.createNewFile()){
+                System.out.println("Archivo creado: " + myFile.getName());
+            } else {
+                System.out.println("Archivo ya existe.");
+            }
 
-    try {
-      File myFile = new File("SimpReserv.txt");
-      if (myFile.createNewFile()){
-        System.out.println("Archivo creado: " + myFile.getName());
-      } else {
-        System.out.println("Archivo ya existe.");
-      }
+        } catch (IOException e) {
+            System.out.println("Ocurrio un error al crear el archivo");
+            e.printStackTrace();
+        }
 
-    } catch (IOException e) {
-      System.out.println("Ocurrio un error al crear el archivo");
-      e.printStackTrace();
+        try {
+            FileWriter fileWriter = new FileWriter("SimpReserv.txt");
+            fileWriter.write("Creando un archivo para nuestra aplicacion SimpReserv");
+            fileWriter.close();
+            System.out.println("Se agrego texto al archivo");
+
+        }catch (IOException e){
+            System.out.println("Error al agregarle contenido al archivo");
+            e.printStackTrace();
+
+        }
     }
-
-    try {
-      FileWriter fileWriter = new FileWriter("SimpReserv.txt");
-      fileWriter.write("Creando un archivo para nuestra aplicacion SimpReserv");
-      fileWriter.close();
-      System.out.println("Se agrego texto al archivo");
-
-    }catch (IOException e){
-      System.out.println("Error al agregarle contenido al archivo");
-      e.printStackTrace();
-
-    }
-  }
 }
