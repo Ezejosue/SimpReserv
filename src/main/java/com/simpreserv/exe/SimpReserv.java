559b26f5091a0f6630c3df771493cf63d60e3bbf
Jcb <jcbatwork@gmail.com>
2022-07-08 11:58:30 -0600

    - Implementacion de librerias de creacion de archivo y envio de correo en creacion de reservacion.

--- src/main/java/com/simpreserv/exe/SimpReserv.java
+++ src/main/java/com/simpreserv/exe/SimpReserv.java
@@ -1,8 +1,6 @@
 package com.simpreserv.exe;
 
-import com.controller.ClientMethods;
-import com.controller.ControllerEmployee;
-import com.model.simpreserv.Menu;
+import com.model.simpreserv.*;
 
 import java.text.ParseException;
 
@@ -10,15 +8,8 @@ public class SimpReserv {
 
   public static void main(String[] args) throws ParseException {
 
-    /*CreateFile createFile = new CreateFile();
-    createFile.setCreateFile();
-
-    SendEmail sendEmail = new SendEmail();
-    sendEmail.createAndSendEmail("tonyvasqueza002@gmail.com", "Reserva del hotel Hilton", "En el archivo adjunto esta toda la informacion de su reserva");*/
-
     Menu login = new Menu();
     login.initialize();
-
 
   }
 }
