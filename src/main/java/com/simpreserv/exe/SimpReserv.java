bd34955a3bbf1048c9376d57cc7331ab7d45b1ac
Tony Vasquez <tonyva002@hotmail.com>
2022-07-13 22:17:16 -0400

    Create Loger.java

--- src/main/java/com/simpreserv/exe/SimpReserv.java
+++ src/main/java/com/simpreserv/exe/SimpReserv.java
@@ -1,5 +1,8 @@
 package com.simpreserv.exe;
 
+import com.controller.ClientMethods;
+import com.controller.HotelMethods;
+import com.controller.RoomMethods;
 import com.model.simpreserv.*;
 
 import java.text.ParseException;
@@ -8,8 +11,29 @@ public class SimpReserv {
 
   public static void main(String[] args) throws ParseException {
 
+    Loger loger = new Loger();
+    Loger.createLogger();
+
+   /* HotelMethods hotelMethods = new HotelMethods();
+    Hotel hotel = hotelMethods.loadRecord();
+
+    ClientMethods clientMethods = new ClientMethods();
+    Client client = clientMethods.findRecordById(1);
+
+    RoomMethods roomMethods = new RoomMethods();
+    Room room = roomMethods.findRecordById(1);
+
+    CreateFile createFile = new CreateFile();
+    createFile.setCreateFile(hotel, client, room);
+
+    SendEmail sendEmail = new SendEmail();
+    sendEmail.createAndSendEmail("tonyvasqueza002@gmail.com", "Reserva del hotel Sea Sand",
+            "En el archivo adjunto esta toda la informacion de su reserva" + "\n" + "\n" + "Saludos cordiales");*/
+
+
     Menu login = new Menu();
     login.initialize();
+
 
   }
 }
